18a793a85376d62a5e9c375e32392adc8bfff2cc
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-01-16 12:01:24 +0530

    Time: 758 ms (10.00%), Space: 115.3 MB (50.00%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        
+        int n=questions.size();
+        vector<long long> dp(n+1,0);
+        long long ans=0;
+        for(int i= n-1; i>=0 ; i--){
+            
+            dp[i]=questions[i][0];
+            
+            int next=questions[i][1];
+            if(i+next+1<n){
+                dp[i]+=dp[i+next+1];
+            }
+            dp[i]=max(dp[i],dp[i+1]);
+            ans=max(dp[i],ans);
+            
+        }
+        return ans;
+        
+    }
+};
