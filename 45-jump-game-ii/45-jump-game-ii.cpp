101e272c640a4c5b90354b4f35f01b29ed309aa7
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-01-20 12:49:48 +0530

    Time: 913 ms (8.28%), Space: 17.3 MB (12.11%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& a) {
+        int n=a.size();
+        vector<int > dp(n,1e9);
+        dp[n-1]=0;
+        for(int i=n-1; i>=0 ; i--){
+            for(int j=i+1 ; j<=a[i]+i and j<n;j++){
+                dp[i]=min(1+dp[j],dp[i]);
+            }
+        }
+        for(auto it : dp)cout<<it<<" ";
+        return dp[0];
+        
+    }
+};
