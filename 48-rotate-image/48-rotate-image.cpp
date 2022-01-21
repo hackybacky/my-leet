3ad98737d5f6a66238ee363e9821dfa14ce04461
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-01-21 13:15:42 +0530

    Time: 6 ms (11.62%), Space: 7.1 MB (73.36%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n=matrix.size();
+        reverse(matrix.begin() ,matrix.end());
+        
+       for(int i=0 ; i< n ;i++){
+           for(int j=i; j< n; j++){
+               
+               swap(matrix[i][j],matrix[j][i]);
+           }
+       }
+        
+    }
+};
