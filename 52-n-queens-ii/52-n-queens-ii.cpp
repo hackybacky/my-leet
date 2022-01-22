52d7ec4ac60ae2fb81ca29971938846c00664429
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-01-22 11:36:20 +0530

    Time: 112 ms (6.79%), Space: 51.7 MB (5.01%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int N;
+    bool check(vector<string > board,int row,int col){
+        
+        for(int k=0 ; k<col ; k++){
+            if(board[row][k]=='Q')return false;
+        }
+        
+        int i=row,j=col;
+        
+        while(i>=0 and j>=0 ){
+            if(board[i][j]=='Q')return false;
+            i--,j--;
+        }
+        i=row,j=col;
+        while(i<N and j>=0){
+            if(board[i][j]=='Q')return false;
+            i++,j--;
+        }
+        
+        return true;
+    }
+    
+    vector<vector<string>> lans;
+    void solve(vector<string> board, int col  ){
+        
+        if(col>=N){lans.push_back(board);}
+        
+        for(int i=0 ; i< N ; i++){
+            
+            if(check(board,i,col)){
+                board[i][col]='Q';
+                (solve(board,col+1));
+                    
+                
+                
+                board[i][col]='.';
+
+            }
+            
+        }
+       // return false;
+        
+    }
+    int totalNQueens(int n) {
+         N=n;
+        vector<string> board(n,string(n,'.'));
+        
+        
+        solve(board,0);
+        return lans.size();
+    }
+};
