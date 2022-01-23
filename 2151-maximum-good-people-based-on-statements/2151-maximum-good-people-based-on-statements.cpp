0320f22e1d843bc84bb4cbc92ea37a296db1fed7
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-01-23 13:23:42 +0530

    Time: 1572 ms (7.69%), Space: 380.3 MB (7.69%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -1,14 +1,11 @@
 class Solution {
-
 public:
-   // int n;
-    bool isvalid(vector<vector<int>> &statements,string cur){
-        int  n=statements.size();
+    int n;
+    bool isvalid(vector<vector<int>> &statements,vector<int> cur){
+        
         for(int i=0 ; i<n ; i++){
-            if(cur[i]=='1'){
-                for(int j=0 ; j<n ; j++){
-                    if(statements[i][j]!=2 and statements[i][j]!=cur[j]-'0')return false;
-                }
+            for(int j=0 ; j<n ; j++){
+                if(cur[i]==1 and (statements[i][j]!=2 and statements[i][j]!=cur[j]))return false;
             }
         }
         
@@ -16,31 +13,41 @@ public:
         
     }
     
-    
+    // int cal(vector<int> cur){
+    //     int ans=0;
+    //     for(int i=0; i<16; i++){
+    //         if(cur[i]==1)ans++;
+    //     }
+    //     return ans;
+    // }
     int ans=0;
-    void solve(vector<vector<int>> &statements,string cur,int i,int an){
+    void solve(vector<vector<int>> &statements,vector<int> cur,int i,int an){
         
-        if(i== statements.size()){
-            if(isvalid(statements,cur))
+        if(i==n){
+            if(isvalid(statements,cur)){
                 ans=max(ans,an);
-            return;
-           
+                return;
+            }
+            else{
+                return ;
+            }
         }
         
-        cur.append(1, '0');
-        solve(statements, cur, i+1, an);        // assuming ith person is bad
-        cur.back() = '1';
-        solve(statements, cur, i+1, an + 1);    // assuming ith person is good
-        cur.pop_back();       
+        cur[i]=1;
+        //ans=0;
+        solve(statements,cur,i+1,an+1);
+        cur[i]=0;
+        solve(statements,cur,i+1,an);
+        
+        
         
         
     }
     int maximumGood(vector<vector<int>>& statements) {
-       
+        n=statements.size();
         ans=0;
-       
-        string cur;
-        solve(statements,"",0,0);
+        vector<int> cur(n+1,0);
+        solve(statements,cur,0,0);
         return ans;
         
     }
