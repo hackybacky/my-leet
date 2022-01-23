4bd7992e2d62515b6b8c0068cdbbc4d9f31b027a
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-01-23 13:24:25 +0530

    Time: 1528 ms (11.54%), Space: 380.6 MB (7.69%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    Solution(){
+          ios_base::sync_with_stdio(false); cin.tie(NULL);
+  cout.tie(NULL);
+
+    }
+public:
     int n;
     bool isvalid(vector<vector<int>> &statements,vector<int> cur){
         
