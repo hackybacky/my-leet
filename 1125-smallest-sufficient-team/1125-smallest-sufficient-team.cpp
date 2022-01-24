5bf828e30444b7ff7853f44a66e7c34f9915b568
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-01-24 17:39:49 +0530

    Time: 232 ms (35.00%), Space: 62.1 MB (31.67%) - LeetHub

---
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {
+        
+        unordered_map<string,int> skill_mp;
+        
+        for(int i=0 ; i<req_skills.size(); i++){
+            skill_mp[req_skills[i]]=i;
+        }
+        int n=req_skills.size();
+        int person_size=people.size();
+        unordered_map<int,vector<int>> dp;
+        dp.reserve(1<<n);
+        dp[0]={};
+        for(int i=0 ; i<person_size ; i++){
+            
+            int cur_skills=0;
+            for(auto it : people[i]){
+                cur_skills |= (1<<(skill_mp[it]));
+            }
+            
+            for(auto it :dp){
+                
+                int comb=(it.first | cur_skills);
+                
+                if(dp.find(comb)==dp.end() || dp[comb].size()>dp[(it.first)].size()+1){
+                    dp[comb]=it.second;
+                    dp[comb].push_back(i);
+                }
+            }
+            
+            
+        }
+        
+       
+        
+    return    (dp[(1<<n)-1]);
+        
+    
+        
+    }
+};
