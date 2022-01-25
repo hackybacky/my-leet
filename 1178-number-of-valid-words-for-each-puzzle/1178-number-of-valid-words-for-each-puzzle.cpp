ddda0d14fbf061358e20645f5f0dd97d230e806e
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-01-25 13:25:22 +0530

    Time: 518 ms (34.56%), Space: 85.8 MB (31.62%) - LeetHub

---
+++ 1178-number-of-valid-words-for-each-puzzle/1178-number-of-valid-words-for-each-puzzle.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int bitmap(string words){
+        int ans=0;
+        for(auto it:words){
+            ans|=(1<<(it-'a'));
+        }
+        return ans;
+    }
+    vector<int> findNumOfValidWords(vector<string>& words, vector<string>& puzzles) {
+        map<int,int> words_map;
+        
+        for(auto it : words){
+            
+            words_map[bitmap(it)]++;
+        }
+        vector<int>ans;
+        for(auto it :puzzles ){
+            int msk , mskcur=bitmap(it);
+            msk=mskcur;
+            int first = bitmap(it.substr(0,1));
+            int cnt=0;
+            while(msk>=1){
+                if( words_map.find(msk)!=words_map.end() and first&msk){
+                    cnt+=words_map[msk];
+                    
+                }
+                msk=(msk-1)&mskcur;
+            }
+            ans.push_back(cnt);
+        }
+        return ans;
+    }
+};
