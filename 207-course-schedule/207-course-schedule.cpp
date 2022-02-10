e2909272059fd13b0aa7beb8a8561d499d9c632d
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-02-10 17:50:28 +0530

    Time: 16 ms (96.50%), Space: 13.4 MB (72.72%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& pre) {
+        int n= pre.size();
+        
+        vector<int> indeg(numCourses+1,0);
+        
+        vector<vector<int>> graph(numCourses+1);
+        for(int i=0; i< n ; i++){
+            int a=pre[i][0];
+            int b=pre[i][1];
+            indeg[a]++;
+            graph[b].push_back(a);
+        }
+        queue<int> q;
+        for(int i=0; i<numCourses; i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+        }
+        
+        
+        int cnt=0;
+        while(!q.empty()){
+            int top=q.front();
+            q.pop();
+            for(auto it : graph[top]){
+                indeg[it]--;
+                if(indeg[it]==0)q.push(it);
+            }
+            cnt++;
+        }
+        cout<<cnt<<endl;
+        return cnt==numCourses;
+        
+    }
+};
