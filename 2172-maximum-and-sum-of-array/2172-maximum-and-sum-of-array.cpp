87f751d1fa27fda25a335b723cecb557daaf2210
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-02-13 16:17:55 +0530

    Time: 478 ms (68.42%), Space: 31.8 MB (68.42%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -1,47 +1,22 @@
 class Solution {
 public:
-    int go(vector<int> &ar, int cur, int n, int k, vector<int> &vis, map<vector<int>, int> &occ){
-        // Hit the last index - base case
-        if(cur >= n)
-            return 0;
-        
-        // Check if the current state is pre-computed
-        if(occ.count(vis))
-            return occ[vis];
-        
-        int mx = -1;
-        
-        // Try filling the slots recursively
-        for(int i = 1; i <= k; i++){
-            
-            // An element can be filled in this 'ith' slot, as each slot can
-            // have atmost 2 elements
-            if(vis[i] < 2){
-                
-                // Increment the filled spaces in 'ith' slot
-                vis[i]++;
-                
-                // Recur to find the best possible answer
-                int res = (i & ar[cur]) + go(ar, cur+1, n, k, vis, occ);
-                
-                // Maximize mx
-                mx = max(mx, res);
-                
-                // Backtrack, empty a space in 'ith' slot
-                vis[i]--;
+
+    int maximumANDSum(vector<int>& a, int k) {
+        int n = a.size();
+        for(int i=0; i<2*k; i++)a.push_back(0);
+        n=2*k;
+        vector<int> dp(1<<n+1,0);
+        int ans=0;
+        for(int i=0; i < (1<<n) ; i++){
+            int cnt=__builtin_popcount(i);
+            int slot =(cnt+1)/2;
+            for(int j=0; j<n; j++){
+                if( (i &(1<<j)) )
+                dp[i]=max(dp[i],dp[ i^(1<<j) ] + (slot & a[j]));
             }
+            ans=max(ans,dp[i]);
         }
         
-        // return
-        return occ[vis] = mx;
-    }
-    int maximumANDSum(vector<int>& ar, int k) {
-        int n = ar.size();
-        map<vector<int>, int> occ;
-        
-        // stores the configuration of slots (left or occupied) (1...k)
-        vector<int> vis(k+1, 0);
-        
-        return go(ar, 0, n, k, vis, occ);
+        return ans;
     }
 };
