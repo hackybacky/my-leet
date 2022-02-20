4dd0d44df08a2a60eda47277749ab779a19f1e28
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-02-20 10:50:33 +0530

    Time: 472 ms (5.04%), Space: 30 MB (7.89%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    static bool cmp(vector<int>a ,vector<int> b){
+        return a[0]<b[0] or (a[0]==b[0] and a[1]>b[1]);
+    }
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        map<int,bool>check;
+        int ans=0;
+        sort(intervals.begin(),intervals.end(),cmp);
+        for(int i=0; i<intervals.size(); i++){
+            int a=intervals[i][0],b=intervals[i][1];
+            for(int j=i+1; j<intervals.size();j++){
+                if(check[j])continue;
+                int da = intervals[j][0],db=intervals[j][1];
+                if(db<=b and !check[j]){
+                    check[j]=true;
+                    //cout<<j<<" "<<i<<" "<<db<<endl;
+                    ans++;
+                }
+            }
+        }
+        return intervals.size()-ans;
+    }
+};
