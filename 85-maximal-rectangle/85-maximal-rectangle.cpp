3e40023a5efba0eff68c7987c8b2878a06ece4c9
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-02-21 15:23:30 +0530

    Time: 278 ms (5.09%), Space: 13 MB (78.27%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& M) {
-        if(!size(M)) return 0;
-        int ans = 0, m = size(M), n = size(M[0]);
-        vector<vector<short>> dp(m+1, vector<short>(n+1));
-        for(int i = m-1; ~i; i--) 
-            for(int j = n-1; ~j; j--) 
-                dp[i][j] = M[i][j] == '1' ? dp[i][j+1] + 1 : 0;
-                    
-        for(int i = 0; i < m; i++) 
-            for(int j = 0; j < n; j++) 
-                for(int row = i, colLen = n; row < m && M[row][j] == '1'; row++)
-                    ans = max(ans, (row-i+1) * (colLen = min(colLen, dp[row][j]*1)));
-                    
+    int maximalRectangle(vector<vector<char>>& a) {
+        int n=a.size();
+        int m= a[0].size();
+        
+        vector<vector<int>> height(n+1,vector<int>(m+1,0));
+        
+        int ans=0;
+        
+        for(int i=0 ; i<n ;i++){
+            for(int j=0; j<m  ;j++){
+                int right=0;
+                for(int k=j; k<m; k++){
+                    if(a[i][k]=='1'){
+                        right++;
+                        height[i][k]=1;
+                        height[i][k]+=(i-1>=0?height[i-1][k]:0);
+                    }
+                    else break;
+                    int d= k-1>=j?height[i][k-1]:1e9;              
+                    int curh= min(height[i][k],d);
+                   // if(i==2 and j==2)cout<<curh<<" "<<right<<endl;
+                    ans=max(ans,curh*right);
+                    height[i][k]=curh;
+                   // if(i==2 and j==0)cout<<curh<<endl;
+                }
+            }
+        }
         return ans;
     }
 };
