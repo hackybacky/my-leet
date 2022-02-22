48afd126e5b56eb8158e9e71310737b8331f6dc9
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-02-22 11:40:59 +0530

    Time: 51 ms (91.43%), Space: 15.3 MB (89.52%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
-        
-        unordered_map<char,int> ch;
-        for(auto it : chars) ch[it]++;
-        int ans=0;
-        
-        for(auto it : words){
-            unordered_map<char,int> cur;
-            
-            for(auto it2 : it)cur[it2]++;
-            
-            for(auto it : cur){
-                if(ch[it.first]<it.second){
-                    goto next;
-                }
-            }
-            ans+=it.size();
-            next:;
-        }
-        return ans;
-    }
+  int cnt[26] = {}, res = 0;
+  for (auto ch : chars) 
+      ++cnt[ch - 'a'];
+  for (auto &w : words) {
+    int cnt1[26] = {}, match = true;
+    for (auto ch : w)
+      if (++cnt1[ch - 'a'] > cnt[ch - 'a']) {
+        match = false;
+        break;
+      }
+    if (match) 
+        res += w.size();
+  }
+  return res;
+}
 };
