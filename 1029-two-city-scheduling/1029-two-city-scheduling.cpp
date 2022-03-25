d65b608879a71e2d6cdba220d55af0b423443cc1
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-03-25 09:47:24 +0530

    Time: 47 ms (5.79%), Space: 17 MB (7.87%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int n;
+    long long dp[101][101][101];
+    long long  recur( int i , int f , int s ,vector<vector<int>>&a){
+        if(i==n){
+            if(f==n/2 and s==f)return 0;
+            else return 1e6;
+        }
+        
+        if(dp[i][f][s]!=-1)return dp[i][f][s];
+        
+        int ans ;
+        
+        ans = min( a[i][0]+recur(i+1,f+1,s,a) , a[i][1]+recur(i+1,f,s+1,a) );
+        
+        return dp[i][f][s]=ans;
+        
+        
+    }
+    
+    int twoCitySchedCost(vector<vector<int>>& a) {
+        n=a.size();
+        memset(dp,-1,sizeof(dp));
+        long long ans = recur(0,0,0,a);
+        return ans;
+    }
+};
