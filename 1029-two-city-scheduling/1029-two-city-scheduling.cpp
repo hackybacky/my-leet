3dc6728e7bcd1c23459f1d9e2b38827c57dd4e69
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-03-25 09:49:26 +0530

    Time: 13 ms (18.08%), Space: 7.8 MB (94.48%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int n;
-    long long dp[101][101][101];
-    long long  recur( int i , int f , int s ,vector<vector<int>>&a){
+    long long dp[101][101];
+    long long  recur( int i , int f  ,vector<vector<int>>&a){
         if(i==n){
-            if(f==n/2 and s==f)return 0;
+            if(f==n/2 )return 0;
             else return 1e6;
         }
         
-        if(dp[i][f][s]!=-1)return dp[i][f][s];
+        if(dp[i][f]!=-1)return dp[i][f];
         
         int ans ;
         
-        ans = min( a[i][0]+recur(i+1,f+1,s,a) , a[i][1]+recur(i+1,f,s+1,a) );
+        ans = min( a[i][0]+recur(i+1,f+1,a) , a[i][1]+recur(i+1,f,a) );
         
-        return dp[i][f][s]=ans;
+        return dp[i][f]=ans;
         
         
     }
@@ -22,7 +22,7 @@ public:
     int twoCitySchedCost(vector<vector<int>>& a) {
         n=a.size();
         memset(dp,-1,sizeof(dp));
-        long long ans = recur(0,0,0,a);
+        long long ans = recur(0,0,a);
         return ans;
     }
 };
