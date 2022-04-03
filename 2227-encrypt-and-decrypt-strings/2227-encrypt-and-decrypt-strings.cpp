b4212ee18c418860547ed1a165e9b9655629a60d
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-04-03 15:10:10 +0530

    Time: 448 ms (76.92%), Space: 91 MB (69.23%) - LeetHub

---
+++ 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.cpp
@@ -0,0 +1,39 @@
+class Encrypter {
+public:
+    map<char,string > en;
+    map<string,int> dc;
+    
+    Encrypter(vector<char>& keys, vector<string>& value, vector<string>& dictionary) {
+        
+        for(int i=0; i<keys.size();i++){
+            en[keys[i]]=value[i];
+        }
+        
+        for(auto it : dictionary){
+            string cur="";
+            for(auto i : it){
+                cur+=en[i];
+            }
+            dc[cur]++;
+        }
+    }
+    
+    string encrypt(string word1) {
+        string cur ="";
+        for(auto it : word1){
+            cur+=en[it];
+        }
+        return cur;
+    }
+    
+    int decrypt(string word2) {
+        return dc[word2];
+    }
+};
+
+/**
+ * Your Encrypter object will be instantiated and called as such:
+ * Encrypter* obj = new Encrypter(keys, values, dictionary);
+ * string param_1 = obj->encrypt(word1);
+ * int param_2 = obj->decrypt(word2);
+ */
