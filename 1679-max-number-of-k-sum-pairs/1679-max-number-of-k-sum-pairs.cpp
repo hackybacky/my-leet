2cd9c972dfe3e63a12d34e2d3eaffad78a011042
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-04 18:05:55 +0530

    Time: 464 ms (5.01%), Space: 74.3 MB (7.04%) - LeetHub

---
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& arr, int k) {
+        map<int,int> freq_hash;
+        
+        for(auto it : arr)freq_hash[it]++;
+        int ans=0;
+        for(auto curele : arr){
+            if(freq_hash[k-curele]>0 and freq_hash[curele]>0){
+                if(k-curele==curele and freq_hash[curele]<2)continue;
+                ans++;
+                freq_hash[curele]--;
+                freq_hash[k-curele]--;
+            }
+        }
+        
+        return ans;
+    
+    }
+};
