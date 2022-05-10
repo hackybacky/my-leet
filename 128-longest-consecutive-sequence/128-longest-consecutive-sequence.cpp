0125958ec2bad20472e6eb65ae22f919d541d4eb
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-10 17:00:31 +0530

    Time: 505 ms (33.68%), Space: 28.7 MB (83.90%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        
+        unordered_set<int>s(nums.begin(),nums.end());
+        int ans = 0;
+        for(auto it : nums){
+            if(s.find(it-1)!=s.end())continue;
+            int j=1;
+            while(s.find(it+j)!=s.end())j++;
+            ans=max(ans,j);
+        }
+        return ans;
      //   for(auto it : nums)mp[it]=1;
         
-        for(auto it : nums ){
-            int cur = it;
-            if(mp.find(it)!=mp.end())continue;
-            if(mp.find(cur-1)!=mp.end() and mp.find(cur+1)!=mp.end()){
-                int maxi = mp[cur+1]+mp[cur-1]+1;
-                
-                mp[cur]=maxi,mp[cur+mp[cur+1]]=maxi,mp[cur-mp[cur-1]]=maxi;
-            }
-            else if(mp.find(cur-1)!=mp.end()){
-                mp[cur]=mp[cur-1]+1;
-                mp[cur-mp[cur-1]]=mp[cur];
-            }
-            else if(mp.find(cur+1)!=mp.end()){
-                mp[cur]=mp[cur+1]+1;
-                mp[cur+mp[cur+1]]=mp[cur];
-            }
-            else mp[cur]=1;
-            //  for(auto it : mp){
-            //     cout<<it.first<<" "<<it.second<<endl;
-            //  }
-            // cout<<"hello"<<endl;
-        }
-        int ans = 0;
-       for(auto it: mp)ans=max(it.second,ans);
-        return ans;
+        
     }
 };
