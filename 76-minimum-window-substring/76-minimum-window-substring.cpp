dd5003aad1e846f397312b267dbca426f2e24265
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-13 19:12:06 +0530

    Time: 34 ms (38.10%), Space: 7.8 MB (49.64%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -11,11 +11,13 @@ public:
         int head=0;
         while(end<n){
             
-            if(count[s[end++]]-->0)counter--;
+            if(count[s[end]]-->0)counter--;
+            end++;
             // end++;
             while(counter==0){
                 if(end-begin<d)d=end-begin,head=begin;
-                if(count.find(s[begin])!=count.end() and count[s[begin++]]++==0)counter++;
+                if(count.find(s[begin])!=count.end() and count[s[begin]]++==0)counter++;
+                begin++;
             }
         }
         return d==INT_MAX? "":s.substr(head, d);
