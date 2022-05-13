d98fb30ade3f9187ca04beff951061d08e59f556
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-14 02:32:00 +0530

    Added solution - LeetHub

---
+++ Broken blocks - GFG/broken-blocks.cpp
@@ -0,0 +1,67 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution {
+public:
+int MaxGold(vector<vector<int>>&matrix){
+    // Code here
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+    int ans=0;
+    for(int i=0 ; i<n ; i++ ){
+        for(int j=0; j<m; j++){
+            if(i==0 ){
+                dp[i][j]=matrix[i][j];
+            }
+            else {
+                if(matrix[i][j]==-1){dp[i][j]=-1;continue;}
+                int a = dp[i-1][j];
+                int b = (j-1>=0?dp[i-1][j-1]:0);
+                int c = (j+1<m ? dp[i-1][j+1]:0);
+                bool flag=false;
+                if(j-1>=0){
+                    if(dp[i-1][j-1]!=-1)flag=true;
+                }
+                if(j+1<m and dp[i-1][j+1]!=-1)flag=true;
+                if(dp[i-1][j]!=-1){flag=true;}
+                if(flag==false){dp[i][j]=-1;continue;}
+               // cout<<"hello"<<endl;
+                dp[i][j]=matrix[i][j];
+                dp[i][j]+=max({a,b,c});
+                
+                //cout<<dp[i][j]<<endl;
+            }
+            //cout<<dp[i][j]<<endl;
+            ans=max(ans,dp[i][j]);
+        }
+    }
+    // int ans =INT_MIN;
+    // for(int i=0; i<m; i++){
+    //     ans = max(ans,dp[n-1][i]);
+    // }
+    return ans;
+}
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int m, n;
+		cin >> m >> n;
+		vector<vector<int>>matrix(m, vector<int>(n, 0));
+		for(int i = 0; i < m; i++){
+			for(int j = 0; j < n; j++)
+				cin >> matrix[i][j];
+		}
+		Solution obj;
+		int ans = obj.MaxGold(matrix);
+		cout << ans <<'\n';
+	}
+	return 0;
+}  // } Driver Code Ends
