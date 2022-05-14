9b72368dbf26dd1af0df7afc82d6ebc7a4f402b2
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-14 23:52:07 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -21,7 +21,7 @@ public:
         if(dp[i][j][t]!=-1)return dp[i][j][t];
         int ans=0;
         for(int k=i+1;k<j;k++){
-            if(s[k]!='T' and s[k]!='F'){
+            // if(s[k]!='T' and s[k]!='F'){
                 
                 int lt = recur(i,k-1,s,1);
                 int lf = recur(i,k-1,s,0);
@@ -41,7 +41,7 @@ public:
                     else ans=(ans+ lf*rf+lt*rt)%mod;
                 }
                 
-            }
+            // }
             
         }
         dp[i][j][t]=ans;
