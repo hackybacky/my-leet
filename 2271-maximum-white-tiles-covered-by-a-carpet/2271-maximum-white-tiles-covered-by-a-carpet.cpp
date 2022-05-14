4e3135e998dd473428b6890402fdba6f9846ad9b
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-15 02:30:17 +0530

    Time: 486 ms (50.00%), Space: 71.6 MB (50.00%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -6,19 +6,21 @@ public:
         int ans = 0;
         vector<int>pref(n+1,0);
         auto p = pref;
+        
         for(int i=0; i<n; i++)p[i]=tiles[i][1];
+        
         for(int i=0; i< n ; i++ ){
             pref[i]+=(i-1>=0?pref[i-1]:0)+(tiles[i][1]-tiles[i][0]+1);
-            auto it = lower_bound(p.begin(),p.begin()+i+1,max(0,tiles[i][1]-carpetlen+1));
+            auto it = lower_bound(p.begin(),p.begin()+i,max(0,tiles[i][1]-carpetlen+1));
             
             auto indi = it-p.begin();
         
             int prev=max(0,(tiles[i][1]-carpetlen+1)-tiles[indi][0]);
-          //  cout<<prev<<" indi "<<i<<" "<<indi<<endl;
+        
             if(indi-1>=0){
                 prev+=pref[indi-1];
             }
-            // cout<<pref[i]-prev<<" "<< i <<" "<<prev<<endl;
+           
             ans=max(ans,pref[i]-prev);
             
         }
