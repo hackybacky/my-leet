178d31d31337287fb35afac63fbf7fa87fb1ef81
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-15 15:31:12 +0530

    Time: 174 ms (16.82%), Space: 59.7 MB (76.50%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,21 +11,29 @@
  */
 class Solution {
 public:
-    map<TreeNode*,int>dis;
-    void dfs(TreeNode *root,TreeNode * par){
-        if(root==nullptr)return;
-        dis[root]=dis[par]+1;
-        dfs(root->left,root);
-        dfs(root->right,root);
+    int d;
+    int ans;
+    int dfs(TreeNode *root,int dis,bool flag){
+        // if(flag)cout<<d<<" "<<dis<<endl;
+       
+        if(root==nullptr)return dis;
+        
+         return max(dfs(root->left,dis+1,flag),
+        dfs(root->right,dis+1,flag));
         
     }
+    void dfs2(TreeNode *root , int dis){
+        if(root==nullptr)return ;
+       
+        if(dis==d-1)ans+=root->val;
+        dfs2(root->left,dis+1);
+        dfs2(root->right,dis+1);
+    }
     int deepestLeavesSum(TreeNode* root) {
-        dfs(root,0);
-        int ans=0;
-        int mini=INT_MIN;
-        for(auto it: dis)mini=max(it.second,mini);
-        for(auto it : dis )(it.second==mini?ans+=it.first->val:0);
-        // cout<<mini<<endl;
+        d=dfs(root,0,false);
+        
+        dfs2(root,0);
+        
         
         return ans;
     }
