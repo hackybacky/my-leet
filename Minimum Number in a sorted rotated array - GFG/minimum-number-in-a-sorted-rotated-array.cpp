96c7b124a4fc5a1524bf62ee22ff61b7693b8a34
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-17 02:27:03 +0530

    Added solution - LeetHub

--- Minimum Number in a sorted rotated array - GFG/minimum-number-in-a-sorted-rotated-array.cpp
+++ Minimum Number in a sorted rotated array - GFG/minimum-number-in-a-sorted-rotated-array.cpp
@@ -18,8 +18,8 @@ class Solution
         while(l<=r){
             int mid =(l+r)/2;
             // cout<<arr[mid]<<" "<<arr[high]<<endl;
-            if(arr[mid]<=arr[high]){r=mid-1;}
-            else l=mid+1;
+            if(arr[mid]>arr[high]){l=mid+1;}
+            else r=mid-1;
         }
         return arr[l];
         
