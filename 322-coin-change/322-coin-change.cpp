402b215736283dbe54357c49dcd292c19c824520
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-21 11:26:32 +0530

    Time: 63 ms (91.68%), Space: 10.8 MB (79.02%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[30000];
+    int recur(vector<int>&coins, int rm ){
+    
+        if(rm ==0)return 0;
+        else if(rm<coins[0] or rm<0)return 1e5;
+        if(dp[rm]!=-1)return dp[rm];
+        int ans=INT_MAX;
+        for(int j=0;j<coins.size(); j++){
+            ans=min(ans,1+recur(coins,rm-coins[j]));
+        }
+        
+        return dp[rm]=ans;
+        
+    }
+    int coinChange(vector<int>& coins, int amount) {
+        memset(dp,-1,sizeof(dp));
+        sort(coins.begin(),coins.end());
+        int ans= recur(coins,amount);
+        return ans >1e4?-1:ans;
+    }
+};
