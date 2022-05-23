a8e0033a8069ef5b7ce541f2433f610525dd6a98
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-23 13:42:05 +0530

    Time: 1952 ms (5.03%), Space: 62.9 MB (29.02%) - LeetHub

---
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int n ,m;
+    int dp[601][2][101][101];
+    int recur(int i , int taken , int tn,int tm,vector<string>&strs){
+        if(i==strs.size())return 0;
+        if(dp[i][taken][tn][tm]!=-1)return dp[i][taken][tn][tm];
+        auto it = strs[i];
+        int one = count(it.begin(),it.end(),'1');
+        int z = it.size()-one;
+        int ans=0;
+        if(tn+one<=n and tm+z<=m){
+            ans=1+recur(i+1,1,tn+one,tm+z,strs);
+        }
+        ans=max(ans,recur(i+1,0,tn,tm,strs));
+        return dp[i][taken][tn][tm]=ans;
+    }
+    int findMaxForm(vector<string>& strs, int M, int N) {
+        m=M;
+        memset(dp,-1,sizeof(dp));
+        n=N;
+       return recur(0,0,0,0,strs);
+    }
+};
