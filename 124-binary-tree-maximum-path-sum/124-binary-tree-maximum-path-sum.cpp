f0d8e541bd79eb4558b9e7bf59209dc18000ac5f
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-26 23:38:09 +0530

    Time: 30 ms (70.43%), Space: 28.8 MB (6.40%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -11,16 +11,16 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode * , int > dp;
+    
     int ans=INT_MIN;
     int  recur( TreeNode * root ){
         if(!root)return 0;
-        if(dp.find(root)!=dp.end())return dp[root];
+       
         int a = recur(root->left);
         int b = recur(root->right);
         int cv=root->val;
         ans=max({ans,a+cv , b+cv, cv , cv+a+b});
-        return  dp[root]=max(cv+max({a,b}),cv);
+        return  max(cv+max({a,b}),cv);
         
     }
     int maxPathSum(TreeNode* root) {
