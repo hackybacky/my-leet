c5c54f7780dd725b60922dd56dc9119a928714b5
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-05-29 17:04:13 +0530

    Time: 645 ms (9.83%), Space: 118.9 MB (15.47%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.cpp
@@ -1,37 +1,28 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-// find_by_order(k)  returns iterator to kth element starting from 0;
-// order_of_key(k) returns count of elements strictly smaller than k;
-//u can find count of element equal to or smaller by using pl and find_by_order({ele,1e18})
- 
+
 class MedianFinder {
 public:
-    ordered_set<pair<int,int>>o;
-    int i;
+    priority_queue<int>small,large;
     MedianFinder() {
-        i=0;
+    
     }
     
     void addNum(int num) {
-        o.insert({num,i++});
+        small.push(-num);
+        large.push(num);
+        
+        if(large.top()>-small.top()){
+            int t = -small.top();
+            small.pop();
+            small.push(-large.top());
+            large.pop();
+            large.push(t);
+        }
     }
     
     double findMedian() {
-       int sz = i;
-        
-        if(sz%2==1){
-            
-            auto it = *(o.find_by_order(sz/2));
-            return it.first;
-        }
-        auto it =*(o.find_by_order(sz/2));
-        auto it2=*(o.find_by_order((sz-1)/2));
-        
-        return (it.first*1.0+it2.first*1.0)/2*1.0;
-        
+       
+        double ans = (large.top()*1.0-small.top()*1.0)/(2.0);
+        return ans;
     }
 };
 
