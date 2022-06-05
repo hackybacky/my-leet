dedd616d347daedd9ba8b7cbbd1761bf47ae943d
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-06-05 13:42:24 +0530

    Time: 724 ms (11.11%), Space: 162.4 MB (33.33%) - LeetHub

---
+++ 2296-design-a-text-editor/2296-design-a-text-editor.cpp
@@ -0,0 +1,72 @@
+class TextEditor {
+public:
+    deque<char>ba,front;
+    TextEditor() {
+        
+    }
+    
+    void addText(string text) {
+        
+        for(auto it : text)ba.push_back(it);
+        
+    }
+    
+    int deleteText(int k) {
+        int ans=0;
+        while(k-- and !ba.empty()){
+            ba.pop_back();ans++;
+        }
+        return ans;
+    }
+    
+    string cursorLeft(int k) {
+        for(int i=0;i<k and ba.empty()==false;i++){
+            front.push_front(ba.back());
+            ba.pop_back();
+        }
+        int i=0;
+        deque<char> v;
+        string ans="";
+        while(!ba.empty() and i<10 ){
+            v.push_front(ba.back());
+            ba.pop_back();
+            i++;
+        }
+        while(!v.empty()){
+            ans+=v.front();
+            ba.push_back(v.front());
+            v.pop_front();
+        }
+        return ans;
+    }
+    
+    string cursorRight(int k) {
+        for(int i=0;i<k and front.empty()==false;i++){
+            ba.push_back(front.front());
+            front.pop_front();
+        }
+        int i=0;
+        deque<char> v;
+        string ans="";
+        while(!ba.empty() and i<10 ){
+            v.push_front(ba.back());
+            ba.pop_back();
+            i++;
+        }
+        while(!v.empty()){
+            ans+=v.front();
+            ba.push_back(v.front());
+            v.pop_front();
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your TextEditor object will be instantiated and called as such:
+ * TextEditor* obj = new TextEditor();
+ * obj->addText(text);
+ * int param_2 = obj->deleteText(k);
+ * string param_3 = obj->cursorLeft(k);
+ * string param_4 = obj->cursorRight(k);
+ */
