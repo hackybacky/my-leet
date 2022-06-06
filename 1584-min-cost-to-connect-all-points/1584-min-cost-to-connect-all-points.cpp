f334c35ad2addda8ac62a3f67c8f0c751765aa9b
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-06-07 00:37:17 +0530

    Time: 1408 ms (13.20%), Space: 58.3 MB (48.05%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -9,12 +9,7 @@ int find_set(int v) {
     void union_sets(int a, int b) {
     a = find_set(a);
     b = find_set(b);
-    if (a != b) {
-        if (size[a] < size[b])
-            swap(a, b);
-        parent[b] = a;
-        size[a] += size[b];
-    }
+    parent[b]=a;
 }
   int minCostConnectPoints(vector<vector<int>>& ps) {
     
