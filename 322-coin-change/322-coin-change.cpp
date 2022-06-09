dcfa043e3441fe3c52c2f7d19b497c40fce8c8c8
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-06-09 16:50:36 +0530

    Time: 161 ms (31.07%), Space: 14.5 MB (35.86%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int dp[10001];
-    int recur(vector<int>&coins, int rm ){
     
-        if(rm ==0)return 0;
-        else if(rm<coins[0] or rm<0)return 1e5;
-        if(dp[rm]!=-1)return dp[rm];
-        int ans=INT_MAX;
-        for(int j=0;j<coins.size(); j++){
-            ans=min(ans,1+recur(coins,rm-coins[j]));
-        }
-        
-        return dp[rm]=ans;
-        
-    }
     int coinChange(vector<int>& coins, int amount) {
-        memset(dp,-1,sizeof(dp));
-        sort(coins.begin(),coins.end());
-        int ans= recur(coins,amount);
-        return ans >1e4?-1:ans;
+        
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        int n = coins.size();
+        
+        for(int i=1;i<=amount;i++){
+            
+            for(int j=0;j <n; j++){
+                int cur = i;
+                int take = cur-coins[j];
+                // cout<<take<<endl;
+                if(take>=0 and dp[take]!=INT_MAX)dp[i]=min(dp[i],1+dp[take]);
+            }
+        }
+        if(dp[amount]==INT_MAX)return -1;return dp[amount];
     }
 };
