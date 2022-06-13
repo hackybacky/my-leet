821474cfc8285b489f969a2f5d401b51840337c7
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-06-13 12:25:13 +0530

    Time: 7 ms (55.27%), Space: 11.5 MB (52.57%) - LeetHub

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dp[5001][2];
+    int recur(int i , int par,vector<int>&prices){
+        if(i>=prices.size()){
+            // if(par==1)return INT_MIN/2;
+            return 0;
+        }
+        
+        if(dp[i][par]!=-1)return dp[i][par];
+        int ans=0;
+        int ans2=recur(i+1,par,prices);
+        
+        if(par==0){
+            
+            ans=-prices[i]+recur(i+1,1,prices);
+        }
+        else{
+            ans=prices[i]+recur(i+2,0,prices);
+        }
+        // cout<<ans<<" "<<ans2<<" "<<i<<endl;
+        return dp[i][par]=max(ans,ans2);
+    }
+    int maxProfit(vector<int>& prices) {
+        memset(dp,-1,sizeof(dp));
+        return recur(0,0,prices);
+    }
+};
