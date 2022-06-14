540445f14422cc4dc376839abfd32124c5a0e6b7
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-06-14 16:29:28 +0530

    Time: 4 ms (71.33%), Space: 6.2 MB (95.93%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[101];
+    int dp[2][101];
     
    
     bool isInterleave(string s1, string s2, string s3) {
@@ -10,23 +10,23 @@ public:
         memset(dp,0,sizeof(dp));
         int n = s1.size();
         int m = s2.size();
-        dp[0]=1;
+        dp[0][0]=1;
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
-                if(i==0 and j==0)dp[j]=1;
+                if(i==0 and j==0)dp[i][j]=1;
                 else if(i==0 ){
-                    dp[j]=((s2[j-1]==s3[j-1+i]) and dp[j-1]);
+                    dp[i&1][j]=((s2[j-1]==s3[j-1+i]) and dp[i&1][j-1]);
                 }
                 else if(j==0 ){
-                    dp[j]=((s1[i-1]==s3[i-1+j]) and dp[j]);
+                    dp[i&1][j]=((s1[i-1]==s3[i-1+j]) and dp[(i-1)&1][j]);
                 
                 }
                 else{
-                    dp[j]=(dp[j-1] and s2[j-1]==s3[i+j-1])||(dp[j] and s1[i-1]==s3[i+j-1]);
+                    dp[i&1][j]=(dp[i&1][j-1] and s2[j-1]==s3[i+j-1])||(dp[(i-1)&1][j] and s1[i-1]==s3[i+j-1]);
                 }
                 
             }
         }
-        return dp[m];
+        return dp[n&1][m];
     }
 };
