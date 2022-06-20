35bb43f3d2612923e70a5674a90890e001334b76
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-06-20 23:50:22 +0530

    Time: 30 ms (18.63%), Space: 13.1 MB (81.72%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -15,12 +15,6 @@ public:
                 }
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                cout<<matrix[i][j]<<" ";
-            }
-            cout<<endl;
-        }
         
         for(int j=1;j<m;j++){
             if(matrix[0][j]==0){
