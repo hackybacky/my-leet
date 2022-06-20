6b773d79a0314d5b5ec106a4183cc02a8f2ce186
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-06-21 00:00:24 +0530

    Time: 30 ms (18.63%), Space: 13.1 MB (95.78%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -16,26 +16,14 @@ public:
             }
         }
         
-        for(int j=1;j<m;j++){
-            if(matrix[0][j]==0){
-                for(int i=0;i<n;i++){
-                    matrix[i][j]=0;
-                }
-            }
-        }
-        
         for(int i=1;i<n;i++){
-            if(matrix[i][0]==0){
-                for(int j=0;j<m;j++)matrix[i][j]=0;
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 or matrix[0][j]==0)matrix[i][j]=0;
+                
             }
         }
-        if(flag)
-        
-            for(int i=0;i<m;i++)matrix[0][i]=0;
-        if(flag2){
-            for(int i=0;i<n;i++)matrix[i][0]=0;
-        }
-        
+        if(flag)for(int i=0;i<m;i++)matrix[0][i]=0;
+        if(flag2)for(int j=0;j<n;j++)matrix[j][0]=0;
         // return matrix;
     }
 };
