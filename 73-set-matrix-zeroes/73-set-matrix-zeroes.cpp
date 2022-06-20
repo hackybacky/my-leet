06d7a1341ade4d30a56bbbb563450d7028794c75
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-06-20 23:49:56 +0530

    Time: 96 ms (5.68%), Space: 13.3 MB (53.19%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        bool flag=false,flag2=false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 and matrix[i][j]==0)flag=true;
+                if(j==0 and matrix[i][j]==0)flag2=true;
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        
+        for(int j=1;j<m;j++){
+            if(matrix[0][j]==0){
+                for(int i=0;i<n;i++){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1;i<n;i++){
+            if(matrix[i][0]==0){
+                for(int j=0;j<m;j++)matrix[i][j]=0;
+            }
+        }
+        if(flag)
+        
+            for(int i=0;i<m;i++)matrix[0][i]=0;
+        if(flag2){
+            for(int i=0;i<n;i++)matrix[i][0]=0;
+        }
+        
+        // return matrix;
+    }
+};
