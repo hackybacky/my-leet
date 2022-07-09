36f05dadf493f6d873665108b0528197d4126290
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-07-09 23:43:18 +0530

    Time: 428 ms (66.67%), Space: 94.1 MB (33.33%) - LeetHub

---
+++ 2334-subarray-with-elements-greater-than-varying-threshold/2334-subarray-with-elements-greater-than-varying-threshold.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int validSubarraySize(vector<int>& a, int threshold) {
+         stack<int>st;
+        int n = a.size();
+        vector<int>ns(n,n);
+        for(int i=0;i<n;i++){
+            int cur = a[i];
+            
+            while(!st.empty() and cur<a[st.top()]){
+                ns[st.top()]=i;
+                st.pop();
+            }
+            st.push(i);
+        }
+        stack<int>stt;
+        vector<int>ps(n,-1);
+        for(int i=n-1;i>=0;i--){
+            int cur = a[i];
+            while(!stt.empty() and cur<a[stt.top()]){
+                ps[stt.top()]=i;
+                stt.pop();
+            }
+            stt.push(i);
+        }
+        
+        for(int i=0;i<n;i++){
+            int l = ps[i];
+            int r = ns[i];
+            int len = r-l-1;
+            // if(len<)
+            double cur_value = threshold*1.0/len*1.0;
+            // cout<<len<<" "<<ns[i]<<" "<<ps[i]<<endl;
+            if(cur_value < (double) a[i]){
+                return len;
+            }
+            
+        }
+        return -1;
+    }
+};
