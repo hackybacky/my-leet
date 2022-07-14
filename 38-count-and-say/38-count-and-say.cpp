858f0705ac06b9eecbf4c6f6fba55564a5a82ad1
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-07-14 23:42:19 +0530

    Time: 7 ms (80.00%), Space: 6.7 MB (55.61%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        string lans="1";
+        
+        for(int i=1;i<n;i++){
+            
+            int cnt=1;
+            string ans = "";
+            char last=lans[0];
+            int sz = lans.size();
+            for(int j=1;j<sz ; j++){
+                if(lans[j]!=last){
+                    ans+=to_string(cnt)+(last);
+                    last=lans[j];
+                    cnt=1;
+                }
+                else cnt++;
+            }
+            ans+=to_string(cnt)+(last);
+            cout<<lans<<endl;
+            lans = ans;
+        }
+        return lans;
+    }
+};
