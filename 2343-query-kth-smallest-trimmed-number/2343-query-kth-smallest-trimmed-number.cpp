2570e4c23e9fca6a92e7088706bf835756416c1d
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-07-21 14:27:27 +0530

    Time: 2963 ms (11.11%), Space: 517.4 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    static bool cmp(pair<string,int>a,pair<string,int>b){
-        return a.first<b.first or (a.first==b.first and a.second<b.second);
+static bool cmp(pair<string,int>a,pair<string,int>b){
+    return a.first<b.first or (a.first==b.first and a.second<b.second);
+}
+int give_ans(int i,vector<string>&nums,int k){
+    
+    vector<pair<string,int>>v;
+    for(auto j=0;j<nums.size();j++ ){
+        auto it = nums[j];
+        int sz = it.size();
+        if(sz-i>=0)
+        v.push_back({it.substr(sz-i),j});
     }
+    sort(v.begin(),v.end());
+    
+    return v[k-1].second;
+}
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
         
-        map<int,vector<pair<string,int>>>mp;
         
-        map<string,int>indi;
-        for(int j=0;j<nums.size();j++){
-            string it=nums[j];
-            int sz = it.size();
-            string t="";
-            for(int i=sz-1;i>=0 ; i--){
-                t=it[i]+t;
-                
-                 mp[sz-i].push_back({t,j});
-            }
-        }
-        
-        for(auto &it:mp){
-            sort(it.second.begin(),it.second.end());
-        }
         vector<int>ans;
         for(auto it : queries){
-            int s = mp[it[1]][it[0]-1].second;
+           
         
             // cout<<s<<endl;
-            ans.push_back(s);
+            ans.push_back(give_ans(it[1],nums,it[0]));
         }
         return ans;
     }
