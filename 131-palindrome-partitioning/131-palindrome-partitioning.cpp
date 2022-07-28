3027c93bc127906dbb88a1cb15b2b73b0ed05d73
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-07-28 17:16:50 +0530

    Time: 170 ms (67.73%), Space: 49.2 MB (90.99%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,30 +1,77 @@
 class Solution {
+
 public:
-    vector<vector<string>>lans;
-    void recur(int i , string &s , vector<string>&ans ){
-        if(i==s.size()){
-            lans.push_back(ans);
-            return ;
+
+    
+
+    bool is_pallindrome(string temp){
+
+        int i=0;
+
+        int j=temp.size()-1;
+
+        while(i<=j){
+
+           if(temp[i]!=temp[j]){
+
+               return false;
+
+           }
+
+            i++;
+
+            j--;
+
         }
-        string cur="";
-        for(int j=i; j < s.size() ; j++){
-            
-            cur+=s[j];
-            
-            string rev = cur;
-            reverse(rev.begin(),rev.end());
-            
-            if(rev==cur){
-                // cout<<cur<<j<<endl;
-                ans.push_back(cur);
-                recur(j+1,s,ans);
+
+        return true;
+
+    }
+
+    
+
+    
+
+    void generate(vector<vector<string>>& matrix,vector<string>& ans,int index,string&s){
+
+        if(index==s.size()){
+
+            matrix.push_back(ans);
+
+            return;
+
+        }
+
+        for(int j=index;j<s.size();j++){
+
+            string temp=s.substr(index,j-index+1);
+
+            if(is_pallindrome(temp)){
+
+                ans.push_back(temp);
+
+                generate(matrix,ans,j+1,s);
+
                 ans.pop_back();
+
             }
+
         }
+
     }
+
+    
+
     vector<vector<string>> partition(string s) {
-        vector<string>a;
-        recur(0,s,a);
-        return lans;
+
+        vector<vector<string>>matrix;
+
+        vector<string>temp;
+
+        generate(matrix,temp,0,s);
+
+        return matrix;
+
     }
+
 };
