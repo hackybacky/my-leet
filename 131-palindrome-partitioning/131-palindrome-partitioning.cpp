e27498fa0299df92175ea2cea2b1b83cb821e89b
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-07-28 11:26:09 +0530

    Time: 546 ms (6.96%), Space: 163.1 MB (13.61%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<string>>lans;
+    void recur(int i , string &s , vector<string>ans ){
+        if(i==s.size()){
+            lans.push_back(ans);
+            return ;
+        }
+        string cur="";
+        for(int j=i; j < s.size() ; j++){
+            
+            cur+=s[j];
+            
+            string rev = cur;
+            reverse(rev.begin(),rev.end());
+            
+            if(rev==cur){
+                cout<<cur<<j<<endl;
+                ans.push_back(cur);
+                recur(j+1,s,ans);
+                ans.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<string>a;
+        recur(0,s,a);
+        return lans;
+    }
+};
