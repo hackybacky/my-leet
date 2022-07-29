098f51f883d062f0045db2505f0bf614874fc1af
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-07-29 15:33:23 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (16.10%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        
+        
+        vector<int>num;
+        int fact=1;
+        for(int i=1;i<n;i++){
+            fact*=i;
+            num.push_back(i);
+        }
+        num.push_back(n);
+        
+        string ans ="";
+        int cur=n-1;
+        k--;
+        while(1){
+            int t = k/fact;
+            ans+=(to_string(num[t]));
+            num.erase(std::remove(num.begin(), num.end(), num[t]), num.end());
+            if(num.size()==0)break;
+            k=k%fact;
+            fact/=num.size();
+            // cur--;
+        }
+        return ans;
+    }
+};
