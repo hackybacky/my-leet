96c058c6649e56fb5653e40c4db8f497e9ef2d7e
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-08-03 13:58:13 +0530

    Time: 176 ms (45.99%), Space: 39.3 MB (17.93%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,43 @@
+class MyCalendar {
+public:
+    map<int,int>mp;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        
+        auto it = mp.lower_bound(end);
+        
+        
+        if((*it).first==end){
+            return false;
+        }
+        if(it!=mp.begin()){
+            auto p = prev(it);
+            int pr = (*p).first;
+            if(start<pr){
+                cout<<"s"<<endl;
+                return false;
+            }
+        }
+        
+        if(it!=mp.end()){
+            auto n = it;
+            if(n!=mp.end()){
+                cout<<"hello"<<endl;
+                int st = (*n).second;
+                if(st<end){cout<<"f"<<endl;return false;}
+            }
+        }
+        mp[end]=start;
+        // auto n = next(it);
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
