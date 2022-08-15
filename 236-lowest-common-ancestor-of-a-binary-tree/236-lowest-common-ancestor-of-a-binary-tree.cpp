d051c3e235afef2ba779d27e2baba8d7a99df78f
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-08-15 16:48:27 +0530

    Time: 20 ms (74.35%), Space: 14.3 MB (57.51%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,14 +9,34 @@
  */
 class Solution {
 public:
+    TreeNode * lca;
+    TreeNode * give_me_lca(TreeNode * root , TreeNode *p , TreeNode *q){
+        if(!root){
+            return root;
+        }
+        if(root==p or root==q)return root;
+        auto flag = give_me_lca(root->left , p , q);
+        auto flag2= give_me_lca(root->right,p,q);
+        
+        if(flag and flag2){
+            // lca=root;
+            return root;
+        }
+        
+        return flag?flag:flag2;
+        
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL or root==p or root==q)return root;
         
-        auto t = lowestCommonAncestor(root->left,p,q);
-        auto t2= lowestCommonAncestor(root->right,p,q);
+        return give_me_lca(root,p,q);
+//         if(root==NULL or root==p or root==q)return root;
         
-        if(t and t2)return root;
+//         auto t = lowestCommonAncestor(root->left,p,q);
+//         auto t2= lowestCommonAncestor(root->right,p,q);
         
-        return t?t:t2;
+//         if(t and t2)return root;
+        
+//         return t?t:t2;
+        return lca;
     }
 };
