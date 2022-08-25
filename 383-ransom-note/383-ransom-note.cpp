ffc499a9846a7527d9dd4807d5c87896d694ab85
hackybacky <65658595+hackybacky@users.noreply.github.com>
2022-08-25 10:15:15 +0530

    Time: 24 ms (56.74%), Space: 9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         
-        map<char,int>mp;
+        unordered_map<char,int>mp;
         for(auto it : magazine){
             mp[it]++;
         }
