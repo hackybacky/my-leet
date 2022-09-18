08655e7b7d629f47210fb60f5d8c8d16b3e4f0d3
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-09-18 10:22:21 +0530

    Time: 2190 ms (12.50%), Space: 149 MB (62.50%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.cpp
@@ -3,11 +3,10 @@ public:
     
     vector<int> sumPrefixScores(vector<string>& words) {
         unordered_map<long long int , int > mp;
-        long long mod = 1e14 + 7;
+        long long mod = 1e12 + 7;
         for(auto it : words){
             long long int key = 0;
-            long long int po = 1;
-            long long p = 29;
+            
             for(int i = 0 ; i < it.size() ; i++){
                 key = (((key<<5) + key) ^ (it[i] - 'a' + 1))%mod;
                  
@@ -17,8 +16,7 @@ public:
         vector<int>ans;
         for(auto it : words){
             long long int key = 0;
-            long long int po = 1;
-            long long int p = 29;
+            
             int c = 0;
             for(int i = 0 ; i < it.size() ; i++){
                 // hash = (hash + (it[i] - 'a' + 1)*po) % m;
