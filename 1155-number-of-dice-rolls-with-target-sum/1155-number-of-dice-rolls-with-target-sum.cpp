894729233771cac32a17c04c26983c5bb953fb3a
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-10-02 17:30:54 +0530

    Time: 41 ms (59.97%), Space: 6.1 MB (87.41%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -18,8 +18,21 @@ public:
     }
     int numRollsToTarget(int n, int k, int target) {
         
-        memset(dp , -1 ,sizeof(dp));
+        // memset(dp , -1 ,sizeof(dp));
+        dp[0][0] = 1;
         
-        return recur(n , target , k);
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 0 ; j <= target ; j++){
+                if(j == 0 and i==0)
+                    dp[i][j] = 1;
+                else{
+                    for(int l = 1 ; l <= k ; l++){
+                        if(j - l >=0 )
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - l])%mod;
+                    }
+                }
+            }
+        }
+        return dp[n][target];
     }
 };
