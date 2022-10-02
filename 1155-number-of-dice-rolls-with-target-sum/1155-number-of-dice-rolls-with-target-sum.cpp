247caeef958611cf4325e4d4d218653cd9e07929
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-10-02 17:31:07 +0530

    Time: 47 ms (53.28%), Space: 6 MB (96.28%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,20 +2,6 @@ class Solution {
 public:
     const int mod = 1e9 + 7;
     int dp[31][1001];
-    int recur(int i , int sum , int k ){
-        if(sum == 0){
-            return i == 0;
-        }
-        if(i == 0 or sum < 0)
-            return 0;
-        if(dp[i][sum] != -1)
-            return dp[i][sum];
-        int ans = 0;
-        for(int j = 1 ; j <= k ;j++){
-            ans = (ans + recur(i - 1 , sum - j , k)) % mod;
-        }
-        return dp[i][sum] = ans;
-    }
     int numRollsToTarget(int n, int k, int target) {
         
         // memset(dp , -1 ,sizeof(dp));
