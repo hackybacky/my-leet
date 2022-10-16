198b4fc2bac5170fd5a48c972cb2f8ccc4ed06ac
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-10-16 18:24:52 +0530

    Time: 18 ms (97.77%), Space: 7.3 MB (93.10%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,29 +1,7 @@
 class Solution {
 public:
     int dp[311][12];
-    int recur(int current_day , int index , vector<int>&dif){
-        if(index == dif.size()){
-            return current_day == 0 ? 0 : 1e6;
-        }
-        
-        if(current_day <=0 ){
-            return 1e6;
-        }
-        
-        if(dp[current_day][index] != -1){
-            return dp[current_day][index];
-        }
-        
-        int maxi =INT_MIN;
-        int ans = INT_MAX;
-        
-        for(int j = index ; j < dif.size() ; j++){
-            maxi = max(maxi , dif[j]);
-            ans = min(ans , maxi + recur(current_day - 1, j + 1 , dif));
-        }
-        
-        return dp[current_day][index] = ans;
-    }
+  
     int minDifficulty(vector<int>& job, int d) {
         memset(dp ,1e6 ,sizeof(dp));
         int n = job.size();
