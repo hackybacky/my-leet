cce0a833bbeb7d60b881dac125d5c5c04a5e9a48
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-10-16 18:25:15 +0530

    Time: 34 ms (91.30%), Space: 7.2 MB (93.10%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -6,19 +6,16 @@ public:
         memset(dp ,1e6 ,sizeof(dp));
         int n = job.size();
         dp[0][0] = 0;
-        // for(int i = 0 ; i <=d ; i++)
-        //     dp[0][j] = 0;
+        
         for(int i = 1 ; i <= n ; i++){
             int maxi = 0;
-            
             for(int j = i ; j <= n ;j++){
                 maxi = max(maxi , job[j - 1]);
                 for(int k = 1 ; k <= d; k++){
-                    // cout  << maxi << endl;
+                    
                     dp[j][k] = min(dp[i - 1][k - 1]  + maxi, dp[j][k]);
                 }
-            }
-            
+            } 
         }
         
         int ans = dp[n][d];
