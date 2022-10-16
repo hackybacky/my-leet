10e098d09d42f3903d1be2fb717c17c48ee885cc
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-10-16 18:24:34 +0530

    Time: 18 ms (97.77%), Space: 7.2 MB (99.15%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -1,28 +1,49 @@
 class Solution {
 public:
-    int dp[11][301];
+    int dp[311][12];
     int recur(int current_day , int index , vector<int>&dif){
         if(index == dif.size()){
             return current_day == 0 ? 0 : 1e6;
         }
+        
         if(current_day <=0 ){
             return 1e6;
         }
+        
         if(dp[current_day][index] != -1){
             return dp[current_day][index];
         }
+        
         int maxi =INT_MIN;
         int ans = INT_MAX;
+        
         for(int j = index ; j < dif.size() ; j++){
             maxi = max(maxi , dif[j]);
             ans = min(ans , maxi + recur(current_day - 1, j + 1 , dif));
         }
+        
         return dp[current_day][index] = ans;
     }
-    int minDifficulty(vector<int>& jobDifficulty, int d) {
-        memset(dp ,-1 ,sizeof(dp));
+    int minDifficulty(vector<int>& job, int d) {
+        memset(dp ,1e6 ,sizeof(dp));
+        int n = job.size();
+        dp[0][0] = 0;
+        // for(int i = 0 ; i <=d ; i++)
+        //     dp[0][j] = 0;
+        for(int i = 1 ; i <= n ; i++){
+            int maxi = 0;
+            
+            for(int j = i ; j <= n ;j++){
+                maxi = max(maxi , job[j - 1]);
+                for(int k = 1 ; k <= d; k++){
+                    // cout  << maxi << endl;
+                    dp[j][k] = min(dp[i - 1][k - 1]  + maxi, dp[j][k]);
+                }
+            }
+            
+        }
         
-        int ans = recur(d , 0 , jobDifficulty);
+        int ans = dp[n][d];
         if(ans >= 1e6){
             return -1;
         }
