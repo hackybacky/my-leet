3d38c57da7dc3964f468c03d29e5117018d28dd6
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-10-17 15:52:18 +0530

    Time: 3 ms (80.43%), Space: 6.4 MB (82.91%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     bool checkIfPangram(string sentence) {
-        
         vector<int>p(26, 0);
-        
         for(auto it : sentence){
             int c = it - 'a';
             p[c] = 1;
-            
         }
         int ans = 0;
         for(int i =0 ; i < 26 ; i++)
             ans += p[i];
-        
         return ans == 26;
     }
 };
