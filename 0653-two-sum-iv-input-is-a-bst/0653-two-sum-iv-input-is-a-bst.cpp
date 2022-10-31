9fceef11a612b3c1c8053391021f29650ee4f8bd
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-10-31 14:51:20 +0530

    Time: 38 ms (96.69%), Space: 36.9 MB (66.86%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    class BSTIterator {
+        public :
+             bool reverse ;
+             stack<TreeNode * > st;
+        BSTIterator(TreeNode * root , bool isReverse){
+            reverse = isReverse;
+            push_node(root);
+        }
+        bool hasNext(){
+            return !st.empty();
+        }
+        int next(){
+            TreeNode * top = st.top();
+            st.pop();
+            if(reverse){
+                push_node(top -> left);
+            }
+            else{
+                push_node(top -> right);
+            }
+            return top -> val;
+        }
+        void push_node(TreeNode * root){
+            while(root){
+                st.push(root);
+                if(reverse){
+                    root = root -> right;
+                }
+                else root = root -> left;
+            }
+        }
+        
+    };
+    bool findTarget(TreeNode* root, int k) {
+        BSTIterator left(root , false);
+        BSTIterator right(root , true);
+        
+        int l = left.next() , r = right.next();
+        while(l < r){
+            int cur = l + r;
+            if(cur < k){
+                l = left.next();
+            }
+            else if(cur > k){
+                r =right.next();
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+};
