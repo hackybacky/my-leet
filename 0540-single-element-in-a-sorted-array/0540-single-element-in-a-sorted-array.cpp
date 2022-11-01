0a8c021936569d136f0938ce8a45ef9f63c56dff
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-11-01 21:50:09 +0530

    Time: 52 ms (62.81%), Space: 22.2 MB (91.52%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& a) {
-        int n = a.size();
-        if(n==1)return a[0];
-        int l = 0 , r = n-1;
-        int ans=-1;
-        while(l<=r){
-            int mid = (l+r)/2;
-            bool o=false,f=false;
-            if(mid%2==1)o=true;
-            bool flag=true;
-            if(mid+1<n and a[mid]==a[mid+1])flag=false;
-            if(mid-1>=0 and a[mid]==a[mid-1])flag=false;
-            if(flag)return a[mid];
-            if(o){
-                if(mid-1>=0 and a[mid]==a[mid-1])l=mid+1;
-                else r=mid-1;
-            }
-            else {
-                if(mid+1<n and a[mid]==a[mid+1])l=mid+2;
-                else r=mid-2;
-            }
-           
-        }
         
-        return a[l-1];
+        int l = 0 , r = a.size()/2;
+        if(a.size() % 2 == 1){
+            r--;
+        }
+        while(l <= r){
+            int mid = (l + r)/2;
+            if(a[2*mid] != a[2*mid + 1]){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return a[2 * l];
     }
 };
