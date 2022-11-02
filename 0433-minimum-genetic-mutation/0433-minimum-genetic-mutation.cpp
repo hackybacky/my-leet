af8df2576b77b94bb81088ce10281e20c1810da0
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-11-02 22:16:07 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (42.79%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        
+        priority_queue<pair<int , string >> pq;
+        map<string , bool > mp;
+        map<string , int > dis;
+        for(auto it : bank){
+            mp[it] = true;
+        }
+        pq.push({0 , start});
+        
+        while(!pq.empty()){
+            
+            auto t = pq.top();
+            int distance = t.first;
+            string s = t.second;
+            if(s == end){
+                return distance ;
+            }
+            // cout << "hello" << endl;
+            pq.pop();
+            for(int i = 0 ; i < s.size() ; i++){
+                char current = s[i];
+                for(char c = 'A' ; c <= 'Z' ; c++){
+                    s[i] = c;
+                    // cout <<s  << endl;
+                    if(mp.find(s) != mp.end() ){
+                        if(dis.find(s) == dis.end())
+                            dis[s] = distance + 1 , pq.push({dis[s] , s}) ;
+                        else if(dis[s] > distance + 1){
+                            dis[s] =distance + 1;
+                            pq.push({dis[s] , s});
+                        }
+                        
+                    }
+                }
+                s[i] = current;
+            }
+        }
+        return -1;
+    }
+};
