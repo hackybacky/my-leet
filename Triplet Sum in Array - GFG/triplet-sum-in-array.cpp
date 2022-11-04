ddbab928f8da3b827a798eaec777f9988d324c56
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-11-04 17:49:59 +0530

    Added solution - LeetHub

---
+++ Triplet Sum in Array - GFG/triplet-sum-in-array.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Function to find if there exists a triplet in the 
+    //array A[] which sums up to X.
+    bool find3Numbers(int A[], int n, int X)
+    {
+        //Your Code Here
+        sort(A , A + n);
+        for(int i = 0 ; i < n - 2 ; i++){
+            
+            int start = i + 1 , end = n - 1;
+            if(i - 1 >=0 and A[i - 1] == A[i]){
+                continue;
+            }
+            int req = X - A[i];
+            
+            while(start < end){
+                int cur = A[start] + A[end];
+                if(cur == req){
+                    return true;
+                }
+                else if(cur < req){
+                    start++;
+                }
+                else{
+                    end--;
+                }
+            }
+        }
+        return false;
+    }
+
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		int n,X;
+		cin>>n>>X;
+		int i,A[n];
+		for(i=0;i<n;i++)
+			cin>>A[i];
+		Solution ob;
+        cout <<  ob.find3Numbers(A, n, X) << endl;
+    }
+}
+
+// } Driver Code Ends
