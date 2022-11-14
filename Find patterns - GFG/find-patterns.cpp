610bbaf4915c74102352dd2cf731109b8c1d7072
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-11-14 18:31:09 +0530

    Added solution - LeetHub

--- Find patterns - GFG/find-patterns.cpp
+++ Find patterns - GFG/find-patterns.cpp
@@ -12,7 +12,7 @@ public:
     int numberOfSubsequences(string s, string w){
         // code here 
         
-        map<int , bool > vis;
+        unordered_map<int , bool > vis;
         int ans = 0 ;
         int n = s.size();
         for(int i = 0 ; i < n ;i++){
