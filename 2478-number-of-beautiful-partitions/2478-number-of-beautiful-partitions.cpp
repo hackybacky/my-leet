b677864a61f54dde753ab28a3037f285b510a693
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-11-20 10:09:28 +0530

    Time: 2208 ms (7.14%), Space: 11.3 MB (71.43%) - LeetHub

--- 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.cpp
+++ 2478-number-of-beautiful-partitions/2478-number-of-beautiful-partitions.cpp
@@ -6,7 +6,8 @@ public:
         return c == '2' or c == '3' or c == '5' or c == '7';
     }
     int recur(int i , string &s , int k , int mini){
-        if(i == s.size()){
+        if(i == s.size() - 1){
+            // cout << s[i] << endl;
             return k == 0;
         }
         if(k == 0 or s.size() - i < mini){
@@ -18,32 +19,26 @@ public:
         // vector<char > v = {'2' , '3' , '5' , '7'};
         
         int ans = 0;
-        if(is_prime(s[i])){
+        
             
+        if(is_prime(s[i])){
             for(int j = i + 1; j < s.size() ;j++){
-                if(is_prime(s[j]) == false and j - i + 1 >= mini){
-                    ans = (ans + recur(j + 1 , s, k - 1, mini )) % mod;
-                }
+            if(is_prime(s[j]) and j - i >= mini and is_prime(s[j - 1]) == false){
+                ans = (ans + recur(j  , s, k - 1, mini )) % mod;
             }
         }
+        }
+        // }
         return dp[i][k] = ans;
         
-        // if(s[i] == '2' or s[i] == '3' or s[i] == '5' or s[i] == '7'){
-        //     int j = i + 1;
-        //     while(j < s.size() and s[j] != '2' and s[j] != '3' and s[j] != '5' and s[j] != '7'){
-        //         j++;
-        //     }
-        //     if(j - i >= mini)
-        //     ans = recur(j , s , k - 1, f , mini);
-        // }
-        
+     
         
         
     }
     int beautifulPartitions(string s, int k, int minLength) {
         
         memset(dp , -1 ,sizeof(dp));
-        
+        s += "7";
         return recur(0 , s , k , minLength);
     }
 };
