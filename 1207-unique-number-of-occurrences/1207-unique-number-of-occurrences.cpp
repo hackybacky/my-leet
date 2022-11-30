c2084ae5d1ad64bc93e55b548a922eafe7cb6245
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-11-30 15:33:28 +0530

    Time: 4 ms (69.41%), Space: 7.9 MB (86.41%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+  short m[2001] = {};
+  for (auto n : arr) ++m[n + 1000];
+  sort(begin(m), end(m));
+  for (auto i = 1; i < 2001; ++i)
+      if (m[i] && m[i] == m[i - 1]) return false;
+  return true;
+}
+};
