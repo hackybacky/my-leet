775e5e88c220b2d8265f9ed4b1f9f08201e29129
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-12-03 15:45:55 +0530

    Added solution - LeetHub

--- Transform String - GFG/transform-string.cpp
+++ Transform String - GFG/transform-string.cpp
@@ -9,20 +9,29 @@ class Solution
 {
     public:
     int transform (string A, string B)
-    {
+    {   int n = A.size();
         //code here.
-        int  j=B.size();
-        string a=A;
-        string b = B;
-        sort(a.begin(),a.end());
-        sort(b.begin(),b.end());
-        if(a!=b)return -1;
-        j--;
-        int ans=0;
-        for(int i=j;i>=0;i--){
-            if(A[i]!=B[j])ans++;
-            else j--;
+        map<int , int > mpa , mpb;
+        for(auto it : A)mpa[it]++;
+        for(auto it : B)mpb[it]++;
+        if(mpa != mpb){
+            return -1;
         }
+        int ans = 0;
+        int j = n - 1 ;
+        int cur = 0;
+        for(int i = n - 1 ;i >=0 and j >= 0  ;i--){
+            if(A[i] == B[j]){
+                ans+= cur;
+                j--;
+                cur = 0;
+            }
+            else{
+                cur++;
+            }
+            
+        }
+        ans += cur;
         return ans;
     }
 };
