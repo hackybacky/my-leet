afc98fdb2be4f5d0954e28c3499b3f07aa38ad6e
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-12-31 15:49:41 +0530

    Time: 3 ms (94.05%), Space: 6.5 MB (78.31%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,26 +1,7 @@
 class Solution {
 public:
     int dp[101][101];
-    bool recur(int i , int j , string & s , string & p){
-        if(i == s.size() and j == p.size())return 1;
-        if(j == p.size())return 0;
-        if(dp[i][j] != -1)return dp[i][j];
-        bool match = (i < s.size() and (s[i] == p[j] or p[j ] == '.'));
-        bool ans = true;
-       if(j + 1< p.size() and p[j + 1] == '*'){
-            if(match){
-                ans &= recur(i + 1, j , s, p) | recur(i + 1 , j + 2, s, p) | recur(i , j + 2, s, p);
-            }
-            else{
-                ans &= recur(i , j + 2 , s, p);
-            }
-        }
-        else{
-            if(match)ans &= recur(i + 1, j +1 ,s , p);
-            else ans = false;
-        }
-        return dp[i][j] = ans;
-    }
+   
     bool isMatch(string s, string p) {
         memset(dp , 0 ,sizeof(dp));
         int n = s.size() , m = p.size();
