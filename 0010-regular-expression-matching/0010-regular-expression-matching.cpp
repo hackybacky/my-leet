a599cf7a770589efefe7f3259ea55d0b902c88f6
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2022-12-31 15:48:13 +0530

    Time: 3 ms (94.05%), Space: 6.4 MB (87.36%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -5,10 +5,6 @@ public:
         if(i == s.size() and j == p.size())return 1;
         if(j == p.size())return 0;
         if(dp[i][j] != -1)return dp[i][j];
-        // if(i == s.size()){
-        //     if(j < p.size() - 2)return false;
-        //     return p[j] == '*' or p[j + 1] == '*';
-        // }
         bool match = (i < s.size() and (s[i] == p[j] or p[j ] == '.'));
         bool ans = true;
        if(j + 1< p.size() and p[j + 1] == '*'){
@@ -26,7 +22,31 @@ public:
         return dp[i][j] = ans;
     }
     bool isMatch(string s, string p) {
-        memset(dp , -1 ,sizeof(dp));
+        memset(dp , 0 ,sizeof(dp));
+        int n = s.size() , m = p.size();
+        dp[n][m] = 1;
+        
+        for(int i = n ; i >= 0  ; i--){
+            for(int j =  m - 1 ; j >= 0 ; j--){
+                bool match = (i < s.size() and (s[i] == p[j] or p[j ] == '.'));
+                 bool ans = true;
+               if(j + 1< p.size() and p[j + 1] == '*'){
+                    if(match){
+                        ans &= dp[i + 1][j] or dp[i + 1][j + 2 ] or dp[i][j + 2];
+                        // ans &= recur(i + 1, j , s, p) | recur(i + 1 , j + 2, s, p) | recur(i , j + 2, s, p);
+                    }
+                    else{
+                        ans &= dp[i][j + 2];
+                        // ans &= recur(i , j + 2 , s, p);
+                    }
+                }
+                else{
+                    if(match)ans &= dp[i + 1][j + 1];
+                    else ans = false;
+                }
+                dp[i][j] = ans;
+            }
+        }
         return recur(0 , 0 , s , p);
     }
 };
