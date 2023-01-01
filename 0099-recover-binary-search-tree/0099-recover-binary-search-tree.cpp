a31a88e466ac2d9ffaffdd4b5cddd5ad34991238
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-01-01 20:27:35 +0530

    Time: 44 ms (70.23%), Space: 57.9 MB (72.68%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,31 +11,35 @@
  */
 class Solution {
 public:
-   TreeNode * first , * second , * third;
-    
-    void inorder(TreeNode * root , TreeNode * & prev){
+    TreeNode * first , * second , * third;
+    void inorder(TreeNode * root , TreeNode * & prv){
         if(!root){
             return ;
         }
-        inorder(root -> left , prev);
-        if(prev and prev -> val > root -> val){
+        inorder(root -> left , prv);
+        if(prv and prv -> val > root -> val){
             if(first == NULL){
-                first = prev , second = root;
+                cout << "hello" << endl;
+                first = prv , second = root;
             }
-            else third = root;
+            else{
+                third = root;
+            }
         }
-        prev = root;
-        inorder(root -> right , prev);
+        prv = root;
+        inorder(root -> right , prv);
     }
-   void recoverTree(TreeNode* root) {
-       TreeNode * prev = NULL;
-       inorder(root , prev);
-       if(first and third){
-           swap(first -> val , third -> val);
-           
-       }
-       else if(first ){
-           swap(first -> val , second -> val);
-       }
+    void recoverTree(TreeNode* root) {
+        TreeNode * prv = NULL;
+        first = NULL , second = NULL , third = NULL;
+        inorder(root , prv);
+        if(third == NULL){
+            // cout << "here my boio";
+            swap(first -> val , second -> val);
+        }
+        else {
+            cout << first -> val << third ->val << endl;
+            swap(first -> val , third -> val);
+        }
     }
 };
