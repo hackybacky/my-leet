919512e7bfc307ab0e15e0466496321c6a0ce003
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-01-01 13:53:14 +0530

    Time: 33 ms (73.68%), Space: 6.5 MB (20.89%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -16,19 +16,7 @@ public:
         return true;
             
     }
-    bool isvalid(vector<vector<char>>& board,int row,int col,char c)
-    {
-        for(int i=0;i<9;i++)
-        {
-            if(board[i][col]==c)
-                return false;
-            if(board[row][i]==c)
-                return false;
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)
-                return false;
-        }
-        return true;
-    }
+   
     bool solve(vector<vector<char>> &board ){
         
         for(int i = 0 ; i < 9 ; i++){
