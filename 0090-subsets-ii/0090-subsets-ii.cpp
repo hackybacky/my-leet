0c771cbe0f9d4bcbf37fa762fe25eced31b1fe27
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-01-01 17:18:13 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (69.15%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> lans;
+    void generate(vector<int>&nums , int i , vector<int>&ans){
+        lans.push_back(ans);
+        
+        for(int j = i ; j  < nums.size() ; j++){
+            
+            if(i != j and nums[j] == nums[j - 1])continue;
+            ans.push_back(nums[j]);
+            generate(nums , j + 1 , ans);
+            ans.pop_back();
+        }
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int>ans;
+        sort(nums.begin() , nums.end());
+        generate(nums , 0 , ans);
+        return lans;
+    }
+};
