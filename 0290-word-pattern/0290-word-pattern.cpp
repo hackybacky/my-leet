af41dbb5abcbbfb074310dcf58f1e5ea2bc89070
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-01-01 11:55:39 +0530

    Time: 4 ms (32.33%), Space: 6.5 MB (19.87%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        
+        stringstream st(s);
+        map<string , int > mp;
+        map<char , int> mp2;
+        int i = 0;
+        string str;
+        while(getline(st , str , ' ')){
+            
+            if(mp[str] == 0 and mp2[pattern[i]] == 0){
+                mp[str] = i + 1;
+                mp2[pattern[i]] = i + 1;
+            }
+            else if(mp[str] != mp2[pattern[i]])return false;
+            i++;
+        }
+        return i == pattern.size();
+        return true;
+    }
+};
