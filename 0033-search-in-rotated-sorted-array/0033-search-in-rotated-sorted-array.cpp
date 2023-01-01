168cece440a355542f72185c4fc4057b3a6a0cbe
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-01-01 13:14:27 +0530

    Time: 3 ms (89.92%), Space: 11 MB (96.34%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,82 +1,25 @@
 class Solution {
 public:
-    //     int search(int A[], int n, int target) {
-    //     int lo=0,hi=n-1;
-    //     // find the index of the smallest value using binary search.
-    //     // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
-    //     // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
-    //     while(lo<hi){
-    //         int mid=(lo+hi)/2;
-    //         if(A[mid]>A[hi]) lo=mid+1;
-    //         else hi=mid;
-    //     }
-    //     // lo==hi is the index of the smallest value and also the number of places rotated.
-    //     int rot=lo;
-    //     lo=0;hi=n-1;
-    //     // The usual binary search and accounting for rotation.
-    //     while(lo<=hi){
-    //         int mid=(lo+hi)/2;
-    //         int realmid=(mid+rot)%n;
-    //         if(A[realmid]==target)return realmid;
-    //         if(A[realmid]<target)lo=mid+1;
-    //         else hi=mid-1;
-    //     }
-    //     return -1;
-    // }
-    int search(vector<int>& A, int target) {
-        int n = A.size();
-        int lo=0,hi=n-1;
-        // find the index of the smallest value using binary search.
-        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
-        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
-        while(lo<hi){
-            int mid=(lo+hi)/2;
-            if(A[mid]>A[hi]) lo=mid+1;
-            else hi=mid;
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int low = 0 , high = n - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            int current_value = nums[mid];
+            int left_value = nums[low] ;
+            int right_value = nums[high];
+            if(current_value == target)return mid;
+            cout << low << " "<< high << " "<< mid << endl;
+            if((nums[mid]-nums[n-1])*(target-nums[n-1])>0){
+                if(current_value < target)low = mid + 1;
+                else high = mid - 1 ;
+            }
+            else if(target > nums[n - 1]){
+                high = mid - 1 ;
+            }
+            else low = mid + 1;
         }
-        // lo==hi is the index of the smallest value and also the number of places rotated.
-        int rot=lo;
-        lo=0;hi=n-1;
-        // The usual binary search and accounting for rotation.
-        while(lo<=hi){
-            int mid=(lo+hi)/2;
-            int realmid=(mid+rot)%n;
-            if(A[realmid]==target)return realmid;
-            if(A[realmid]<target)lo=mid+1;
-            else hi=mid-1;
-        }
-        return -1;
-//         int n = nums.size();
-//         int l =0,r=n-1;
         
-//         while(l <= r){
-//             int mid = (l + r)/2;
-//             if(nums[mid] > nums[r]){
-//                 l = mid + 1;
-//             }
-//             else{
-//                 r = mid - 1;
-//             }
-//         }
-//         int k = l;
-//         int ans = -1;
-//         l = 0 ,r = n - 1;
-//         while(l <= r){
-//             int mid = (l + r)/2;
-//             int nmid = ( mid + k )% n;
-//             // cout << nmid << endl;
-//             if(nums[nmid] == target){
-//                 return ans = nmid ;
-//                 break;
-//             }
-//             if( nums[nmid] < target){
-//                 l = mid + 1;
-//             }
-            
-//             else{
-//                 r = mid - 1;
-//             }
-//         }
-//         return ans;
+        return -1;
     }
 };
