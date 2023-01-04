3b241cb33775ed3e6bc6456ed7071617010136f1
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-01-04 11:50:57 +0530

    Added solution - LeetHub

---
+++ Maximum Profit By Choosing A Subset Of Intervals - GFG/maximum-profit-by-choosing-a-subset-of-intervals.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    static bool cmp(vector<int>&a , vector<int>&b){
+        return a[1] < b[1] or (a[1] == b[1] and a[2] >= b[2]);
+    }
+    int maximum_profit(int n, vector<vector<int>> &intervals) {
+        // Write your code here.
+        sort(intervals.begin() , intervals.end() , cmp);
+        map<int , int > dp;
+        dp[0] = 0;
+        int maxi = 0;
+        // dp[intervals[0][1]] = intervals[0][2];
+        for(int i = 0 ; i < n ; i++){
+            auto it2 = intervals[i];
+            int start = it2[0] , end = it2[1] , profit = it2[2];
+            auto it = dp.upper_bound(start );
+            
+            if(it != dp.begin()){
+                it = prev(it);
+                
+                dp[end] = max(profit + (*it).second , maxi);
+            }
+            maxi = max(dp[end] , maxi);
+            // else dp[end] = max(profit , (*dp.rbegin()).second);
+            
+        }
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> intervals(n, vector<int>(3));
+        for (int i = 0; i < n; i++) {
+            cin >> intervals[i][0] >> intervals[i][1] >> intervals[i][2];
+        }
+        Solution obj;
+        cout << obj.maximum_profit(n, intervals) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
