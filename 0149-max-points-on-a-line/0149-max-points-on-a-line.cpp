9203bb91c1f79d2ec10144e250ae23dd99ec4c87
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-01-08 11:03:05 +0530

    Time: 36 ms (88.22%), Space: 12.5 MB (70.67%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
-        map<double, int > mp;
+        map<pair<int , int>, int > mp;
         sort(points.begin() , points.end());
         int n = points.size();
         int ans = 0 ;
         for(int i = 0 ; i < n ;i++){
             mp.clear();
             for(int j = i + 1 ; j < n ;j++){
-                double x = points[j][0] - points[i][0];
-                double y = points[j][1] - points[i][1];
+                int x = points[j][0] - points[i][0];
+                int y = points[j][1] - points[i][1];
                 
-                
-                mp[y / x]++;
+                int g = __gcd(x , y);
+                x /= g;
+                y /= g;
+                mp[{x , y}]++;
             }
             for(auto it : mp){
                 ans = max(ans , it.second);
