40ea500a03c7649cb6bee24bc6052f6d29f3b5bd
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-01-13 12:10:07 +0530

    Time: 585 ms (72.62%), Space: 215.4 MB (26.41%) - LeetHub

--- 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
+++ 2246-longest-path-with-different-adjacent-characters/2246-longest-path-with-different-adjacent-characters.cpp
@@ -1,39 +1,32 @@
 class Solution {
 public:
-   
-    vector<vector<pair<int,char>>>graph;
-    int ans;
-    
-    int dfs(int node , int par,char c){
-        multiset<int>v;
-        for(auto child :graph[node]){
-            if(child.first==par)continue;
-            
-            if(c==child.second){
-                dfs(child.first,node, child.second);
-                continue;
+    vector<vector<int>> graph;
+    int ans = 0;
+    int dfs(int node , int par , string & s){
+        int cur = 1;
+        int a = 0 , b = 0;
+        for(auto it : graph[node]){
+            if(it == par)continue;
+            int dis_child = dfs(it , node , s);
+            if(s[it] != s[node]){
+                cur = max(cur , dis_child + 1);
+                if(dis_child > a)b = a , a= dis_child ;
+                else if(dis_child > b)b = dis_child;
             }
-            int cur = dfs(child.first,node, child.second);
-            v.insert(cur+1);
-            if(v.size()>2)v.erase(v.begin());
         }
-        int su=0,maxi=0;
-        for(auto it :v){
-            su+=it;
-            maxi=max(maxi,it);
-        }
-        ans=max(ans,su);
-        return maxi;
+        // cout << node << "a "<< a << " b " << b << endl;
+        ans = max(ans ,a + b + 1);
+        ans = max(ans , cur);
+        return cur;
     }
     int longestPath(vector<int>& parent, string s) {
         int n = s.size();
-        
-        graph.resize(n+1,{});
-        for(int i=1;i<parent.size();i++){
-            graph[parent[i]].push_back({i,s[i]});
-            graph[i].push_back({parent[i],s[i]});
+        graph.assign(n , {});
+        for(int i = 1 ; i < n ; i++){
+            graph[i].push_back(parent[i]);
+            graph[parent[i]].push_back(i);
         }
-        dfs(0,-1,s[0]);
-        return ans+1;
+        dfs(0 , -1 , s);
+        return ans;
     }
 };
