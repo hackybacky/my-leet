885f5f6c78292b5190c941707dd83f60572d6c84
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-06-24 19:02:34 +0530

    Time: 1694 ms (24.64%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,3 @@
+select Signups.user_id , ROUND(AVG(if(Confirmations.action = "confirmed" , 1 , 0)) , 2) as confirmation_rate from Signups left join Confirmations on Signups.user_id = Confirmations.user_id group by Signups.user_id  
+
+
