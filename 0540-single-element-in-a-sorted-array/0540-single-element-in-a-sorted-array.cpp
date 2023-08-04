42c8d0fcb0c661ec729ea699e6e675e0f0ad31ab
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-08-04 22:41:52 +0530

    Time: 18 ms (96.62%), Space: 22.5 MB (5.69%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,10 +4,12 @@ public:
         
         int l = 0 , r = nums.size();
         if(r == 1)return nums[0];
+        int ans = nums[0];
         while(l <= r){
             int mid = (l + r)/2;
             if(mid % 2 == 1){
                 if(nums[mid] != nums[mid - 1]){
+                    ans = nums[mid];
                     r = mid - 1;
                 }
                 else l = mid + 1;
@@ -15,10 +17,11 @@ public:
             else{
                 if(nums[mid + 1] != nums[mid]){
                     r = mid - 1;
+                    ans = nums[mid];
                 }
                 else l = mid + 1;
             }
         }
-        return nums[l];
+        return ans;
     }
 };
