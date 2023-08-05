8c8d71846b988fec9ae107507be5eeddb9e43bfb
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-08-05 16:16:51 +0530

    Time: 176 ms (9.44%), Space: 45.5 MB (72.54%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
-    int partition(vector<int>&nums , int low , int high){
-        
-        int pivot = nums[high];
-        int i = low - 1;
-        for(int k = low  ;  k < high ; k++){
-            if(nums[k] < pivot){
-                swap(nums[k] , nums[++i]);
+    int mergeSort(vector<int>&nums , int l , int r ,int mid){
+        int j = l - 1;
+        for(int i = l ; i < r ; i++){
+            if(nums[i] < nums[r] ){
+                swap(nums[++j] , nums[i]);
             }
         }
-        swap(nums[high] , nums[i + 1] );
-        // for(auto it : nums)cout << it << " ";
-        // cout << endl;
-        return i + 1;
         
+        swap(nums[++j] , nums[r]);
+        return j  ;
     }
     int findKthLargest(vector<int>& nums, int k) {
         
         int n = nums.size();
-        k = n - k;
-        int low = 0 , high = n - 1;
-        while(low <= high){
-            int part = partition(nums , low , high);
-            if(part == k ){
-                return nums[k ];
+        if(n == 1)return nums[0];
+        int l = 0 , r = n - 1;
+        k = n - k ;
+        cout << k << endl;
+        while(l <= r){
+            int mid = (l + r )/2;
+            int check = mergeSort(nums , l , r , mid);
+            if(check < k){
+                l = check + 1;
             }
-            else if(part < k ){
-                low = part + 1;
-            }
-            else high = part - 1;
+            else if(check == k)return nums[k];
+            else r = check - 1;
         }
         return -1;
-        
-        
     }
 };
