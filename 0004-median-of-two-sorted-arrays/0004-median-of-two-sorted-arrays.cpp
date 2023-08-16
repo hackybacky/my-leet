ae750c6f42cf5bda1568ec8af49309b129f45992
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-08-16 16:55:12 +0530

    Time: 18 ms (96.34%), Space: 89.2 MB (74.99%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size()  , m = nums2.size();
+        if(n > m)swap(n , m) , swap(nums1 , nums2);
         
-        int n=nums1.size();
-        int m=nums2.size();
-        if(m<n)swap(n,m) ,swap(nums1,nums2);
+        int l = 0 , r = n ; 
         
-        int l=0,r=n;
-        
-        while(l<=r){
+        while(l <= r){
+            int mid = (l + r)/2;
+            int rem = (n + m + 1)/2 - mid;
             
-            int cut1= (l+r)>>1;
-            int cut2= (n+m+1)/2-cut1;
+            int left = mid > 0 ? nums1[mid - 1] : INT_MIN;
+            int left2 = rem > 0 ? nums2[rem - 1] : INT_MIN;
+            int right = mid < n ? nums1[mid] : INT_MAX;
+            int right2 = rem < m ? nums2[rem] : INT_MAX;
             
-            int left1= cut1==0 ? INT_MIN :nums1[cut1-1];
-            int left2= cut2==0 ? INT_MIN :nums2[cut2-1];
-            
-            int right1= cut1>=n ? INT_MAX : nums1[cut1];
-            int right2= cut2>=m ? INT_MAX : nums2[cut2];
-            
-            if(max(left1 , left2) <= min(right1 , right2)){
-                if((n+m)%2==0)return ( max(left1,left2) + min(right1 , right2 ))/2.0;
-                else return max(left1,left2);
+            if(max(left , left2) <= min(right , right2)){
+                if( (n+ m ) %2 == 0){
+                    return (max(left , left2) + min(right , right2))*1.0 /2;
+                }
+                return min(max(left , left2)  , min(right , right2));
             }
-            else if(left1>right2)r=cut1-1;
-            else l=cut1+1;
-            
-            
+            else{
+                if(left > right2)r = mid - 1;
+                else l = mid + 1;
+            }
         }
-        
-        return 0.0;
-        
+        return -1;
     }
 };
