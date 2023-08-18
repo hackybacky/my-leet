8c2dae0fd22a55e6306ca9879404a6164f651e5a
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-08-18 22:54:53 +0530

    Time: 993 ms (7.39%), Space: 19.5 MB (66.85%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -5,13 +5,13 @@ class Solution:
         n = len(mat)
         m = len(mat[0])
         ans =  [[10**17 for j in range(m)] for i in range(n)]
-        print(ans)
+        # print(ans)
         for i in range(n):
             for j in range(m):
                 if mat[i][j] != 1:
                     q.put([i , j , 0])
                     ans[i][j] = 0
-                    print(i , j , ans[i][j])
+    
             
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
         while q.empty() == False:
@@ -22,10 +22,8 @@ class Solution:
             for j in range(4):
                 nx = x + directions[j][0]
                 ny = y + directions[j][1]
-                # print(nx , ny , ans[nx][ny])
                 if nx < 0 or nx >= n or ny < 0 or ny >= m or ans[nx][ny] <= d + 1:
                     continue
-                # print("f")
                 ans[nx][ny] = d +1
                 # print(nx , ny)
                 q.put([nx , ny , d + 1])
