8e4b6b94d5f4061db628090a429ca218ce141677
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-08-25 23:05:36 +0530

    Time: 21 ms (59.19%), Space: 13.6 MB (55.93%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+            
+        string ss = s + s;
+        ss = ss.substr(1 , ss.size() - 2);
+        // return ;
+        return  ss.find(s) != -1;
+    }
+};
