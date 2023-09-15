6d16c03c452e8910405a319090d6f4a49d58c3c4
Hareesh Kumar <65658595+hackybacky@users.noreply.github.com>
2023-09-15 21:02:45 +0530

    Time: 679 ms (41.06%), Space: 58.5 MB (54.90%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1584-min-cost-to-connect-all-points/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1584-min-cost-to-connect-all-points.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int>parent,size;
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+    void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    parent[b]=a;
+}
+  int minCostConnectPoints(vector<vector<int>>& ps) {
+    
+        priority_queue<pair<int,pair<int,int>>>pq;
+      
+        int n= ps.size();
+      parent.assign(n+1,0);
+      size=parent;
+      for(int i=0; i<n; i++)parent[i]=i,size[i]=1;
+      for(int i=0 ; i<n; i++){
+          for(int j=i+1;j<n;j++){
+              int d = abs(ps[i][0]-ps[j][0])+abs(ps[i][1]-ps[j][1]);
+              pq.push({-d,{i,j}});
+          }
+      }
+      int ans=0;
+      while(!pq.empty()){
+          auto t = pq.top();
+          pq.pop();
+          int d = -t.first;
+          int x = t.second.first;
+          int y = t.second.second;
+          
+          int px = find_set(x),py=find_set(y);
+          if(px!=py){
+              union_sets(x,y);
+              ans+=d;
+          }
+          
+      }
+      return ans;
+    }
+};
